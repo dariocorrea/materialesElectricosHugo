6605ac2b11629cde8de8305c92e5f0c700f69c9c
Darío Javier Correa <dcorrea@emeal.nttdata.com>
2024-07-30 12:38:30 -0300

    Se cambia conexion a DB

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-	host: 'mysql-darioakd.alwaysdata.net',
-	user: 'darioakd',
-	password: 'vetbiDl8]HcY4Sbh',
-	database: 'darioakd_matelectricos',
-	port: 3306,
+	host: 'monorail.proxy.rlwy.net',
+	user: 'root',
+	password: 'BTzVPQwwjWiRIFqbWXvCQmrpuCygecTd',
+	database: 'railway',
+	port: 47643,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
